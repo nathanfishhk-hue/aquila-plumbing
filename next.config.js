6d0ccf64eb7a9a50b0c63a7e68d21e02c00df209
nathanfishhk-hue <nathan.fish.hk@gmail.com>
2026-05-28 10:15:01 +0200

    Fix Next.js config for v15 compatibility

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 const nextConfig = {
-  experimental: {
-    reactRoot: true,
-  },
   images: {
-    domains: ['mixkit.co', 'images.unsplash.com', 'lh3.googleusercontent.com'],
-  },
-  webpack: (config) => {
-    config.externals.push({
-      'utf-8-validate': 'commonjs utf-8-validate',
-      'bufferutil': 'commonjs bufferutil',
-    });
-    return config;
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'mixkit.co',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+    ],
   },
 };
 
